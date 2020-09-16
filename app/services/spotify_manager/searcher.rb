20f418ef4aa9d7f60f2dfc4c9727c26a6029db99
Marcelo <marcelo18a@msn.com>
2020-09-15 21:25:17 -0300

    add spotify searcher service tests

---
+++ app/services/spotify_manager/searcher.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+module SpotifyManager
+  class Searcher < ApplicationService
+  end
+end
---
+++ spec/services/spotify_manager/searcher_spec.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe SpotifyManager::Searcher, type: :model do
+  describe 'calling the object' do
+    context 'with valid options' do
+      # TODO: Stub API Call
+      let!(:result) { SpotifyManager::Searcher.call }
+
+      it 'returns an OpenStruct object' do
+        expect(result).to be_an_instance_of(OpenStruct)
+      end
+
+      it 'responds to success? which is a boolean value' do
+        expect(result).to respond_to(:success?)
+        expect(result.success?).to be true
+      end
+
+      it 'responds to payload which is a Hash' do
+        expect(result).to respond_to(:payload)
+        # expect(result.payload).to be_an_instance_of(Hash)
+      end
+
+      # it 'returns the authentication token' do
+      #   expect(result.payload[:access_token]).not_to be_empty
+      #   expect(result.payload[:access_token]).to be_an_instance_of(String)
+      # end
+    end
+  end
+end
