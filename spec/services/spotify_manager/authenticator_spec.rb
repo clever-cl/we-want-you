8819f8f18b1932e3b2a1ff47c030045ada2847f0
Marcelo <marcelo18a@msn.com>
2020-09-15 21:22:02 -0300

    add more tests

--- spec/services/spotify_manager/authenticator_spec.rb
+++ spec/services/spotify_manager/authenticator_spec.rb
@@ -4,19 +4,28 @@ require 'rails_helper'
 
 RSpec.describe SpotifyManager::Authenticator, type: :model do
   describe 'calling the object' do
-    let!(:result) { SpotifyManager::Authenticator.call }
+    context 'with valid options' do
+      # TODO: Stub API Call
+      let!(:result) { SpotifyManager::Authenticator.call }
 
-    it 'returns an OpenStruct object' do
-      expect(result).to be_an_instance_of(OpenStruct)
-    end
+      it 'returns an OpenStruct object' do
+        expect(result).to be_an_instance_of(OpenStruct)
+      end
 
-    it 'responds to success? which is a boolean value' do
-      expect(result).to respond_to(:success?)
-      expect(result.success?).to be_in([true, false])
-    end
+      it 'responds to success? which is a boolean value' do
+        expect(result).to respond_to(:success?)
+        expect(result.success?).to be true
+      end
 
-    it 'responds to payload' do
-      expect(result).to respond_to(:payload)
+      it 'responds to payload which is a Hash' do
+        expect(result).to respond_to(:payload)
+        expect(result.payload).to be_an_instance_of(Hash)
+      end
+
+      it 'returns the authentication token' do
+        expect(result.payload[:access_token]).not_to be_empty
+        expect(result.payload[:access_token]).to be_an_instance_of(String)
+      end
     end
   end
 end
