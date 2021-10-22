d7fe8a500c1fcabc6c522029fc5e7ca25b2bee6d
Manuel Vera <manuel.vera.poblete@gmail.com>
2021-10-21 22:48:57 -0300

    login spotify

--- src/views/Home/index.js
+++ src/views/Home/index.js
@@ -1,22 +1,33 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import logo from './style/Spotify-logotipo.jpg';
 import './style/App.css';
+import { useLocation } from 'react-router';
+
+
+const apiSpotifyUrl = `https://accounts.spotify.com/authorize?client_id=${process.env.REACT_APP_CLIENT_ID}&response_type=code&redirect_uri=${process.env.REACT_APP_CALLBACK_HOST}&scope=user-read-private`;
 
 export default function Home() {
+    const location = useLocation();
+
+    useEffect(() => {
+        const urlParams = new URLSearchParams(Location.search);
+        const spoCode = urlParams.get("code");
+        console.log(spoCode);
+    },[]);
+
+    const handleLoginClick = () => {
+        window.location.replace(apiSpotifyUrl);
+    };
     return(
         <div className="App">
             <header className="App-header">
                 <img src={logo} className="App-logo" alt="logo" />
                 <p>Buscador de Albunes</p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    >
-                    Iniciar Secion
-                </a>
+
+                <button onClick={handleLoginClick}>Iniciar sesion</button>
+
+                 
             </header>
         </div>
     );
